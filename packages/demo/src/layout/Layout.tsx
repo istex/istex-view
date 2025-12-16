43bcf7432c1dbcb92ea9d467fcd8c3b83a793187
ThieryMichel <thiery@marmelab.com>
2025-12-16 11:25:57 +0100

    add NavBar to layout

--- packages/demo/src/layout/Layout.tsx
+++ packages/demo/src/layout/Layout.tsx
@@ -1,8 +1,10 @@
 import Header from "./Header";
+import Navbar from "./NavBar";
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
+			<Navbar />
 			<Header />
 			{children}
 		</>
