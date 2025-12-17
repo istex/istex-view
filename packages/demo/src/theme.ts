1544ad60713011d4c0487fb3cbc2d1b7387cc5d5
ThieryMichel <thiery@marmelab.com>
2025-12-17 14:11:24 +0100

    add lineheight in theme for title

--- packages/demo/src/theme.ts
+++ packages/demo/src/theme.ts
@@ -5,31 +5,37 @@ export default createTheme({
 		h1: {
 			font: "montserrat",
 			fontSize: "2.5rem",
+			lineHeight: "3.75rem",
 			fontWeight: "bold",
 		},
 		h2: {
 			font: "montserrat",
 			fontSize: "2.25rem",
+			lineHeight: "3.375rem",
 			fontWeight: "bold",
 		},
 		h3: {
 			font: "montserrat",
 			fontSize: "2rem",
+			lineHeight: "3rem",
 			fontWeight: "bold",
 		},
 		h4: {
 			font: "montserrat",
 			fontSize: "1.75rem",
+			lineHeight: "2.625rem",
 			fontWeight: "bold",
 		},
 		h5: {
 			font: "montserrat",
 			fontSize: "1.5rem",
+			lineHeight: "2.25rem",
 			fontWeight: "bold",
 		},
 		h6: {
 			font: "montserrat",
 			fontSize: "1.25rem",
+			lineHeight: "1.875rem",
 			fontWeight: "bold",
 		},
 		body1: {
