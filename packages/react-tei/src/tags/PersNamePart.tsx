9b7f350f5f0f0b5dd24aeb6445b8ecf63d48450e
ThieryMichel <thiery@marmelab.com>
2025-12-18 11:59:06 +0100

    add test on ersNamePart

--- packages/react-tei/src/tags/PersNamePart.tsx
+++ packages/react-tei/src/tags/PersNamePart.tsx
@@ -3,29 +3,66 @@ import { useTranslation } from "react-i18next";
 import type { DocumentJson } from "../parser/document.js";
 import { Value } from "./Value.js";
 
-type PersNameProps = {
+export const getDescriptionKey = (data: DocumentJson) => {
+	const { tag, attributes } = data;
+
+	if (tag === "roleName") {
+		if (
+			!attributes?.["@type"] ||
+			!["honorific", "degree"].includes(attributes["@type"])
+		) {
+			console.warn("PersNamePart roleName missing or invalid @type:", data);
+			return null;
+		}
+		return attributes["@type"];
+	}
+
+	if (
+		[
+			"genName",
+			"nameLink",
+			"roleName",
+			"forename",
+			"surname",
+			"addName",
+			"orgName",
+		].includes(tag)
+	) {
+		return tag;
+	}
+
+	console.warn("PersNamePart unknown tag:", data);
+	return null;
+};
+
+export type PersNamePartProps = {
 	data: DocumentJson;
 };
 
-export const PersNamePart = ({ data }: PersNameProps) => {
+export function PersNamePart({ data }: PersNamePartProps) {
 	const { t } = useTranslation();
 	if (!Array.isArray(data.value)) {
 		console.warn("PersName data.value is not an array:", data.value);
 		return null;
 	}
 
-	const descriptionType = useMemo(() => {
-		if (data.tag === "roleName") {
-			return data.attributes?.["@type"] ? data.attributes["@type"] : "roleName";
-		}
+	if (data.value.length === 0) {
+		console.warn("PersNamePart data.value is empty:", data);
+		return null;
+	}
 
-		return data.tag;
+	const descriptionType = useMemo(() => {
+		return getDescriptionKey(data);
 	}, [data]);
 	return (
-		<span aria-description={t(`sidebar.author.${descriptionType}`)}>
+		<span
+			aria-description={
+				descriptionType ? t(`sidebar.author.${descriptionType}`) : undefined
+			}
+		>
 			{data.value.map((item, index) => (
 				<Value key={index} data={item} />
 			))}
 		</span>
 	);
-};
+}
