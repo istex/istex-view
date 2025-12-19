92ab2ba7ef4f94e1c9486d2f0ef181e592062a95
Jonathan ARNAULT <jonathan@marmelab.com>
2025-12-19 14:36:42 +0100

    Apply review comment

--- packages/react-tei/src/tags/Hi.tsx
+++ packages/react-tei/src/tags/Hi.tsx
@@ -1,87 +1,93 @@
 import { Typography } from "@mui/material";
-import { type ReactNode, useCallback, useMemo } from "react";
+import { type ReactNode, useMemo } from "react";
 import type { ComponentProps } from "./type.js";
 import { Value } from "./Value.js";
 
+function RecursiveHi({
+	rendList: [format, ...rest],
+	children,
+}: RecursiveHiProps): ReactNode {
+	if (!format) {
+		return children;
+	}
+
+	const recursiveChildren = (
+		<RecursiveHi rendList={rest}>{children}</RecursiveHi>
+	);
+
+	switch (format) {
+		case "italic":
+			return <Typography component="em">{recursiveChildren}</Typography>;
+		case "bold":
+			return <Typography component="strong">{recursiveChildren}</Typography>;
+		case "underline":
+			return (
+				<Typography
+					component="u"
+					sx={{
+						textDecoration: "underline",
+					}}
+				>
+					{children}
+				</Typography>
+			);
+		case "superscript":
+			return (
+				<Typography
+					component="sup"
+					sx={{
+						verticalAlign: "super",
+						fontSize: "smaller",
+						marginLeft: "-0.25em",
+					}}
+				>
+					{recursiveChildren}
+				</Typography>
+			);
+		case "subscript":
+			return (
+				<Typography
+					component="sub"
+					sx={{
+						verticalAlign: "sub",
+						fontSize: "smaller",
+						marginLeft: "-0.25em",
+					}}
+				>
+					{recursiveChildren}
+				</Typography>
+			);
+		case "smallCaps":
+			return (
+				<Typography
+					component="span"
+					sx={{
+						fontVariant: "small-caps",
+					}}
+				>
+					{recursiveChildren}
+				</Typography>
+			);
+		default:
+			console.warn(`Unknown format: ${format}`);
+			return recursiveChildren;
+	}
+}
+
+type RecursiveHiProps = {
+	rendList: string[];
+	children: ReactNode;
+};
+
 export function Hi({ data: { value, attributes } }: ComponentProps) {
-	const rend = useMemo(
+	const rendList = useMemo(
 		() => (attributes?.["@rend"] ? attributes["@rend"].split(" ") : []),
 		[attributes],
 	);
 
-	const renderFormat = useCallback(
-		(format: string, children: React.ReactNode) => {
-			switch (format) {
-				case "italic":
-					return <Typography component="em">{children}</Typography>;
-				case "bold":
-					return <Typography component="strong">{children}</Typography>;
-				case "underline":
-					return (
-						<Typography
-							component="u"
-							sx={{
-								textDecoration: "underline",
-							}}
-						>
-							{children}
-						</Typography>
-					);
-				case "superscript":
-					return (
-						<Typography
-							component="sup"
-							sx={{
-								verticalAlign: "super",
-								fontSize: "smaller",
-								marginLeft: "-0.25em",
-							}}
-						>
-							{children}
-						</Typography>
-					);
-				case "subscript":
-					return (
-						<Typography
-							component="sub"
-							sx={{
-								verticalAlign: "sub",
-								fontSize: "smaller",
-								marginLeft: "-0.25em",
-							}}
-						>
-							{children}
-						</Typography>
-					);
-				case "smallCaps":
-					return (
-						<Typography
-							component="span"
-							sx={{
-								fontVariant: "small-caps",
-							}}
-						>
-							{children}
-						</Typography>
-					);
-				default:
-					return children;
-			}
-		},
-		[],
+	return (
+		<RecursiveHi rendList={rendList}>
+			<Value data={value} />
+		</RecursiveHi>
 	);
-
-	const recursiveRender = useCallback(
-		(rend: string[]): ReactNode => {
-			const [format, ...rest] = rend;
-			if (!format) {
-				return <Value data={value} />;
-			}
-
-			return renderFormat(format, recursiveRender(rest));
-		},
-		[renderFormat, value],
-	);
-
-	return recursiveRender(rend);
 }
