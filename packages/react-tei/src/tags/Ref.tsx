df04708ed0349852edfd73fb27cb402a14622500
ThieryMichel <thiery@marmelab.com>
2025-12-22 14:27:38 +0100

    add link in ref

--- packages/react-tei/src/tags/Ref.tsx
+++ packages/react-tei/src/tags/Ref.tsx
@@ -1,6 +1,17 @@
+import { Link } from "@mui/material";
 import type { ComponentProps } from "./type";
 import { Value } from "./Value";
 
-export function Ref({ data: { value } }: ComponentProps) {
-	return <Value data={value} />;
+export function Ref({ data: { value, attributes } }: ComponentProps) {
+	return (
+		<Link
+			component="button"
+			onClick={() => {
+				// TODO: implement scrolling to target
+				// console.log(attributes?.["@target"]);
+			}}
+		>
+			<Value data={value} />
+		</Link>
+	);
 }
