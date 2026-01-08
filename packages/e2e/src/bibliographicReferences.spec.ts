5d91ebdda14487cc8008717ee2fc02d6aa4b1784
Jonathan ARNAULT <jonathan@marmelab.com>
2026-01-08 11:11:10 +0100

    Fix E2E

--- packages/e2e/src/bibliographicReferences.spec.ts
+++ packages/e2e/src/bibliographicReferences.spec.ts
@@ -1,7 +1,7 @@
 import test, { expect } from "@playwright/test";
 import { launchViewer, uploadFile } from "./support/upload";
 
-test("document bibliographic references panel", async ({ page }) => {
+test("scroll to bib reference from text", async ({ page }) => {
 	await page.goto("/");
 	await uploadFile(page, "bibliographicReferences.tei");
 	await launchViewer(page);
@@ -59,7 +59,13 @@ test("document bibliographic references panel", async ({ page }) => {
 		page.getByRole("button", { name: "Ouvrir le panneau latéral" }),
 	).toBeVisible();
 
-	await page.getByText("You shall not pass !").click();
+	const refButton = page.getByRole("button", {
+		name: "You shall not pass !",
+	});
+
+	await refButton.scrollIntoViewIfNeeded();
+	await refButton.click();
+
 	await expect(
 		page.getByRole("button", { name: "Références bibliographiques (3)" }),
 	).toHaveAttribute("aria-expanded", "true");
@@ -81,16 +87,25 @@ test("document bibliographic references panel", async ({ page }) => {
 	await expect(
 		bibRefSection.getByText("Foundation Isaac Aasimov 1957 GnomePress 5"),
 	).toBeVisible();
+});
 
-	await expect(
-		page.getByText("Life is great without it you'd be dead."),
-	).toBeVisible();
-	await expect(
-		page.getByText("Life is great without it you'd be dead."),
-	).not.toBeInViewport();
+test("scroll into document from bib reference", async ({ page }) => {
+	await page.goto("/");
+	await uploadFile(page, "bibliographicReferences.tei");
+	await launchViewer(page);
 
-	await page.getByLabel("Gummo, Korine Harmony 1997 2:423-424").click();
-	await expect(
-		page.getByText("Life is great without it you'd be dead."),
-	).toBeInViewport();
+	const textWithBibRef = page.getByText(
+		"Life is great without it you'd be dead.",
+	);
+	await expect(textWithBibRef).toBeVisible();
+	await expect(textWithBibRef).not.toBeInViewport();
+
+	const gummoRef = page
+		.getByLabel("Références bibliographiques (3)")
+		.getByRole("button", { name: "Gummo, Korine Harmony 1997 2:423-424" });
+	await expect(gummoRef).toBeVisible();
+	await gummoRef.scrollIntoViewIfNeeded();
+	await gummoRef.click();
+
+	await expect(textWithBibRef).toBeInViewport();
 });
