c9efc23676c2f3fa19f4d2138fe9647286fb50ed
Jonathan ARNAULT <jonathan@marmelab.com>
2026-01-12 14:55:46 +0100

    Fix(abstract): Do not render abstract with head=Highlights

---
+++ packages/react-tei/src/abstract/useDocumentAbstracts.tsx
@@ -0,0 +1,28 @@
+import { useMemo } from "react";
+import { findChildrenByName } from "../helper/findChildrenByName";
+import { findTagByName } from "../helper/findTagByName";
+import type { DocumentJson } from "../parser/document";
+
+export const UNSUPPORTED_ABSTRACT_HEAD = "highlights";
+
+export function useDocumentAbstracts(teiHeader: DocumentJson | undefined) {
+	return useMemo(() => {
+		if (!teiHeader) {
+			return [];
+		}
+		const profileDesc = findTagByName(teiHeader, "profileDesc");
+		const abstracts = findChildrenByName(profileDesc, "abstract");
+
+		return abstracts.filter((abstract) => {
+			const head = findTagByName(abstract, "head", 2);
+			const headContent = findTagByName(head, "#text", 2);
+			if (typeof headContent?.value !== "string") {
+				return true;
+			}
+
+			return !UNSUPPORTED_ABSTRACT_HEAD.includes(
+				headContent.value.toLowerCase(),
+			);
+		});
+	}, [teiHeader]);
+}
