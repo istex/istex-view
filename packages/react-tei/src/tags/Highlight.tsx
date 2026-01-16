e77884d79ab4f2cc1c7c8d8bd19ef4204e987c3c
Jonathan ARNAULT <jonathan@marmelab.com>
2026-01-16 11:45:52 +0100

    Feat(unitex): Add toggle support for terms

--- packages/react-tei/src/tags/Highlight.tsx
+++ packages/react-tei/src/tags/Highlight.tsx
@@ -1,21 +1,42 @@
 import { Box } from "@mui/material";
+import { useMemo } from "react";
+import { useDocumentContext } from "../DocumentContextProvider";
 import { chipColors } from "../SidePanel/unitex/unitexAnnotationBlocks";
 import type { ComponentProps } from "./type";
 import { Value } from "./Value";
 
-export const Highlight = ({ data }: ComponentProps) => {
+export const Highlight = ({ data }: HighlightProps) => {
 	const { value, attributes } = data;
-	const groups = Array.isArray(attributes?.groups)
-		? attributes?.groups
-		: attributes?.groups
-			? [attributes?.groups]
-			: [];
+	const { unitexEnrichment } = useDocumentContext();
+
+	const groups = useMemo(() => {
+		if (!attributes?.groups || !unitexEnrichment || typeof value !== "string") {
+			return [];
+		}
+		return ([] as string[]).concat(attributes?.groups).filter((group) => {
+			const term = unitexEnrichment?.document?.[group]?.find(
+				(term) => term.term === value,
+			);
+
+			return term?.displayed ?? false;
+		});
+	}, [unitexEnrichment, attributes?.groups, value]);
+
+	if (groups.length === 0) {
+		return <Value data={value} />;
+	}
+
 	return (
 		<Box
-			component="span"
+			component="mark"
 			data-term={attributes?.term}
 			data-group={groups.join(" ")}
+			role="mark"
 			sx={{
+				color: "inherit",
+				backgroundColor: "transparent",
+			}}
+			style={{
 				boxShadow: groups
 					.map((g, index) =>
 						index === 0
@@ -28,4 +49,13 @@ export const Highlight = ({ data }: ComponentProps) => {
 			<Value data={value} />
 		</Box>
 	);
+};
+
+type HighlightProps = {
+	data: Omit<ComponentProps["data"], "attributes"> & {
+		attributes?: {
+			groups?: string[] | string;
+			term?: string;
+		};
+	};
 };
