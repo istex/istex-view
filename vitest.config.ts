9346f11804169dffa0db60463b4267ce349585a6
Clément Dreptin <clement.dreptin@inist.fr>
2026-03-20 11:34:46 +0100

    chore(deps): replaced @vitejs/plugin-react-swc with @vitejs/plugin-react since it's rolldown based now

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { playwright } from "@vitest/browser-playwright";
 import { defineConfig } from "vitest/config";
 
