2967db4a7a26fcc90d9623d1b88e31a62b2f013f
Clément Dreptin <clement.dreptin@inist.fr>
2026-03-20 11:22:35 +0100

    chore(deps): removed unused vite-tsconfig-paths package

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -1,20 +1,13 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import vitePluginDevtoolsJson from "vite-plugin-devtools-json";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
 	base: process.env.GITHUB_ACTIONS ? "/istex-view/" : undefined,
 	server: {
 		port: 3000,
 	},
-	plugins: [
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		vitePluginDevtoolsJson(),
-		react(),
-	],
+	plugins: [vitePluginDevtoolsJson(), react()],
 });
 
 export default config;
